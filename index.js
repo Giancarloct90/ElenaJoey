e5d9612e03a1fd5c7e1b297620f255754863ca30
giancarloct90 <giancarloct90@hotmail.com>
2021-03-10 08:02:37 -0600

    final

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const navLinks = document.getElementById('navLinks');
 const burger = document.getElementById('burger');
+const navs = document.querySelectorAll('.navLinks li');
 
 burger.addEventListener('click' ,()=>{
     navLinks.classList.toggle('navLinksActive');
     burger.classList.toggle('toggle');
+
 });
+navs.forEach(link=>{
+    link.addEventListener('click', ()=>{
+        console.log('hello');
+        navLinks.classList.toggle('navLinksActive');
+        burger.classList.toggle('toggle');
+    })
+});
+
+
